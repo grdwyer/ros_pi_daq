7124b47c5e8008752e2142bc88523936f2167a4e
George Dwyer <george.dwyer@ucl.ac.uk>
2021-07-14 12:42:40 +0000

    Added service to setup and remove pins for headless version

--- daq_server/daq_server/daq_server.py
+++ daq_server/daq_server/daq_server.py
@@ -2,7 +2,7 @@ import rclpy
 from rclpy.node import Node
 from gpiozero import LED, Button
 from daq_interfaces.msg import GPIOValues
-from daq_interfaces.srv import SetGPIOValues
+from daq_interfaces.srv import SetGPIOValues, ModifyGPIOSetup
 
 
 class DAQServer(Node):
@@ -19,6 +19,8 @@ class DAQServer(Node):
         self.publisher_inputs = self.create_publisher(GPIOValues, "/daq_server/inputs", 10)
         self.publisher_outputs = self.create_publisher(GPIOValues, "/daq_server/outputs", 10)
         self.service_set_output = self.create_service(SetGPIOValues, "/daq_server/set_output", self.callback_set_output)
+        self.service_setup_pins = self.create_service(ModifyGPIOSetup, "/daq_server/configure_pins",
+                                                      self.callback_configure_pins)
         self.timer_publish_input = None
         self.get_logger().info("Configuring GPIO pins")
         self.configure_pins()
@@ -68,7 +70,7 @@ class DAQServer(Node):
                     response.success = False
             else:
                 self.get_logger().warn("Received request to set pin {} which is not setup as an output".format(pin))
-                response.message += "\nfailed to set pin {} as it is not setup".format(pin)
+                response.message += "\nfailed to set pin {} as it is not setup as an output".format(pin)
                 response.success = False
 
         return response
@@ -87,6 +89,86 @@ class DAQServer(Node):
             msg.value.append(self.output_pins[pin].value)
 
         self.publisher_outputs.publish(msg)
+
+    def callback_configure_pins(self, request: ModifyGPIOSetup.Request, response: ModifyGPIOSetup.Response):
+        # Check the pin numbers are the same length as operations otherwise return false
+        if len(request.pin_numbers) == len(request.operations):
+            fails = 0
+            for pin, operation in zip(request.pin_numbers, request.operations):
+                # Check the pin and request makes sense either
+                #    * in assigned_pins and remove
+                #    * not in assigned and input or output
+
+                if pin in self.assigned_pins and operation == ModifyGPIOSetup.Request.REMOVE:
+                    # Request to remove pin received
+                    # Find the pin in input or output
+                    if pin in self.input_pins:
+                        self.input_pins.pop(pin)
+                        self.assigned_pins.remove(pin)
+                        msg = "Pin {} removed from input pins \n".format(pin)
+                        response.message += msg
+                        self.get_logger().info(msg)
+                    elif pin in self.output_pins:
+                        self.output_pins[pin].off()
+                        self.output_pins.pop(pin)
+                        self.assigned_pins.remove(pin)
+                        msg = "Pin {} removed from output pins".format(pin)
+                        response.message += msg
+                        self.get_logger().info(msg)
+                    else:
+                        self.assigned_pins.remove(pin)
+                        msg = "Pin {} was assigned but not found as input or output, removing from assigned pins".format(pin)
+                        response.message += msg
+                        self.get_logger().warn(msg)
+                        fails +=1
+
+                elif pin not in self.assigned_pins and operation == ModifyGPIOSetup.Request.INPUT:
+                    msg = "Adding pin {} as Input".format(pin)
+                    self.get_logger().info(msg)
+                    response.message += msg
+                    self.input_pins[pin] = Button(pin, pull_up=True)
+                    self.assigned_pins.append(pin)
+
+                elif pin not in self.assigned_pins and operation == ModifyGPIOSetup.Request.OUTPUT:
+                    msg = "Adding pin {} as output".format(pin)
+                    self.get_logger().info(msg)
+                    response.message += msg
+                    self.output_pins[pin] = LED(pin)
+                    self.assigned_pins.append(pin)
+
+                # Incorrect setups (can't be done things for warnings)
+                elif pin in self.assigned_pins and (operation == ModifyGPIOSetup.Request.OUTPUT or
+                                                    operation == ModifyGPIOSetup.Request.INPUT):
+                    msg = "Pin {} is already assigned, please remove the pin first".format(pin)
+                    self.get_logger().warn(msg)
+                    response.message += msg
+                    fails += 1
+
+                elif pin not in self.assigned_pins and operation == ModifyGPIOSetup.Request.REMOVE:
+                    msg = "Pin {} is not assigned and therefore can't be removed".format(pin)
+                    self.get_logger().warn(msg)
+                    response.message += msg
+                    fails += 1
+
+                else:
+                    msg = "This case had not been considered, request was pin {} and operation {}\nOutput is 1, " \
+                          "Input is 2 and Remove is 3".format(pin, operation)
+                    self.get_logger().warn(msg)
+                    response.message += msg
+                    fails += 1
+
+            if fails > 0:
+                response.success = False
+                self.get_logger().warn("Failed to modify {} pins out of {} requests".format(fails, len(request.pin_numbers)))
+            else:
+                response.success = True
+        else:
+            msg = "Recieved {} pins to change and {} operations. The values need to be the same.".format(
+                len(request.pin_numbers), len(request.operations))
+            self.get_logger().warn(msg)
+            response.message += msg
+            response.success = False
+        return response
 
 
 def main(args=None):
